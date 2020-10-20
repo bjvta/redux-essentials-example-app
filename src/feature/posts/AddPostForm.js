a639fdf0052ec0150421841c703dc9de0920cfec
Brandon Jason <bjvtamayo78@gmail.com>
2020-10-19 21:23:51 -0400

    Set the post with the API

--- src/feature/posts/AddPostForm.js
+++ src/feature/posts/AddPostForm.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector} from "react-redux";
-import { postAdded } from "./postsSlice";
+import { addNewPost } from "./postsSlice";
+import { unwrapResult} from "@reduxjs/toolkit";
 
 export const AddPostForm = () => {
     const [title, setTitle] = useState("")
     const [content, setContent] = useState("")
     const [userId, setUserId] = useState('')
+    const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
     const dispatch = useDispatch()
 
@@ -15,15 +17,26 @@ export const AddPostForm = () => {
     const onContentChanged = e => setContent(e.target.value)
     const onAuthorChanged = e => setUserId(e.target.value)
 
-    const onSavePostClicked = () => {
-        if (title && content){
-            dispatch(postAdded(title, content, userId))
-            setTitle('')
-            setContent('')
+    const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle'
+
+    const onSavePostClicked = async () => {
+        if (canSave) {
+            try {
+                setAddRequestStatus('pending')
+                const resultAction = await dispatch(
+                    addNewPost({title, content, user: userId})
+                )
+                unwrapResult(resultAction)
+                setTitle('')
+                setContent('')
+                setUserId('')
+            } catch (err) {
+                console.log('Failed to save the post: ', err)
+            } finally {
+                setAddRequestStatus('idle')
+            }
         }
     }
-
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
 
     const usersOptions = users.map(user => (
         <option key={user.id} value={user.id}>
