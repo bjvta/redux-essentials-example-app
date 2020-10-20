7c2f924e10e8607e5ec763c071d424e8e1664464
Brandon Jason <bjvtamayo78@gmail.com>
2020-10-19 21:02:17 -0400

    Update the post list loading from API

--- src/feature/posts/postsSlice.js
+++ src/feature/posts/postsSlice.js
@@ -1,11 +1,16 @@
-import {createSlice, nanoid} from '@reduxjs/toolkit';
-import {sub} from 'date-fns'
+import {createSlice, nanoid, createAsyncThunk} from '@reduxjs/toolkit';
+import {client} from "../../api/client";
 
 const initialState = {
     posts: [],
     status: 'idle',
     error: null
 }
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const response = await client.get('/fakeApi/posts')
+    return response.posts
+})
 
 const postsSlice = createSlice({
     name: 'posts',
@@ -45,9 +50,22 @@ const postsSlice = createSlice({
         reactionAdded(state, action) {
             const {postId, reaction} = action.payload
             const existingPost = state.posts.find(post => post.id === postId)
-            if (existingPost){
+            if (existingPost) {
                 existingPost.reactions[reaction]++
             }
+        }
+    },
+    extraReducers: {
+        [fetchPosts.pending]: (state, action) => {
+            state.status = 'loading'
+        },
+        [fetchPosts.fulfilled]: (state, action) => {
+            state.status = 'succeeded'
+            state.posts = state.posts.concat(action.payload)
+        },
+        [fetchPosts.rejected]: (state, action) => {
+            state.status = 'failed'
+            state.error = action.error.message
         }
     }
 })
